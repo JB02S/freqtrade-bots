ebe0f1571a14f5c58ac42c321aeafbee09a22383
JB02S <james.a.browne442@gmail.com>
2024-01-29 16:08:58 +0000

    Added logic for ema crossover strategy

--- strategies/EMACrossover.py
+++ strategies/EMACrossover.py
@@ -9,7 +9,12 @@ import talib.abstract as ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 
 
-class Simple(IStrategy):
+class EMACrossover(IStrategy):
+
+    """
+    EMA Crossover strategy, uses two emas, when the shorter EMA crosses over the longer EMA the strategy
+    longs, and shorts when short EMA crosses down on the longer EMA
+    """
 
     INTERFACE_VERSION: int = 3
     minimal_roi = {
@@ -24,33 +29,44 @@ class Simple(IStrategy):
 
         ema50 = ta.EMA(dataframe, timeperiod=50)
         ema200 = ta.EMA(dataframe, timeperiod=200)
-        dataframe['ema50'] = ema50['ema']
-        dataframe['ema200'] = ema200['ema']
-
-        bollinger = qtpylib.bollinger_bands(dataframe['close'], window=12, stds=2)
-        dataframe['bb_lowerband'] = bollinger['lower']
-        dataframe['bb_upperband'] = bollinger['upper']
-        dataframe['bb_middleband'] = bollinger['mid']
+        # print(type(ema50))
+        dataframe['ema50'] = ema50
+        dataframe['ema200'] = ema200
 
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        # Long condition, ema50 crosses up on ema200
         dataframe.loc[
             (
-                (
-                        (dataframe['macd'] > 0)  # over 0
-                        & (dataframe['macd'] > dataframe['macdsignal'])  # over signal
-                        & (dataframe['bb_upperband'] > dataframe['bb_upperband'].shift(1))  # pointed up
-                        & (dataframe['rsi'] > 70)  # optional filter, need to investigate
-                )
+                (dataframe['ema50'] > dataframe['ema200'])
             ),
             'enter_long'] = 1
+
+        # Short condition, ema50 crosses down on ema200
+        # dataframe.loc[
+        #     (
+        #         (dataframe['ema50'] < dataframe['ema200'])
+        #     ),
+        #     'enter_short'] = 1
+
         return dataframe
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        # Exit long condition, ema 50 crosses down on ema200
         dataframe.loc[
             (
-                (dataframe['rsi'] > 80)
+                (dataframe['ema50'] < dataframe['ema200'])
             ),
             'exit_long'] = 1
+
+        # Exit short condition, ema 50 crosses up on ema200
+        # dataframe.loc[
+        #     (
+        #         (dataframe['ema50'] > dataframe['ema200'])
+        #     ),
+        #     'exit_short'] = 1
+
         return dataframe
