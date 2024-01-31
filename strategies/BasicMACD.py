f3221a00419e755e9e8695ee0fcb83f806fe8d9a
JB02S <the_real_one_7000@proton.me>
2024-01-31 22:10:37 +0000

    Added basic strategy logic for basic macd + stochastic strategy

--- strategies/BasicMACD.py
+++ strategies/BasicMACD.py
@@ -5,7 +5,6 @@ from pandas import DataFrame
 
 import talib.abstract as ta
 
-
 class BasicMACD(IStrategy):
 
 
@@ -14,27 +13,48 @@ class BasicMACD(IStrategy):
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
-        macd, macdsignal, macdhist = ta.MACD(dataframe, fastperiod=12, slowperiod=26, signalperiod=9)
-        dataframe['macd'] = macd
-        dataframe['macdsignal'] = macdsignal
-        dataframe['macdhist'] = macdhist
+        # MACD settings
+        macd = ta.MACD(dataframe, fastperiod=12, slowperiod=26, signalperiod=9)
+        dataframe['macd'] = macd['macd']
+        dataframe['macdsignal'] = macd['macdsignal']
+        dataframe['macdhist'] = macd['macdhist']
 
+        # Stochastic settings
+        stoch = ta.STOCH(dataframe, fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3,
+                                slowd_matype=0)
+        dataframe['slowk'] = stoch['slowk']
+        dataframe['slowd'] = stoch['slowd']
+        print("\n\n\n\n")
+        print(dataframe)
+        print("\n\n\n\n")
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # Place holder
+
+        # TODO: Improve logic on this strategy so it enters only on crossovers and also add macd part of strat
         dataframe.loc[
             (
-                (dataframe['close'] > dataframe['close'].shift(1))
+                (dataframe['slowk'] < 20) &
+                (dataframe['slowd'] < 20) &
+                (dataframe['slowk'].shift(1) < dataframe['slowd'].shift(1)) &
+                (dataframe['slowk'] > dataframe['slowd']) &
+                (dataframe['macdhist'] > 0)
+                # (dataframe['macd'] > dataframe['macdsignal']),
             ),
             'enter_long'] = 1
+
         return dataframe
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # Place holder
+
+        # TODO: Improve logic on this strategy so it exits only on crossovers and also add macd part of strat
         dataframe.loc[
             (
-                (dataframe['close'] < dataframe['close'].shift(1))
+                (dataframe['slowk'] > 80) &
+                (dataframe['slowd'] > 80) &
+                (dataframe['slowk'] < dataframe['slowd']) &
+                (dataframe['slowk'].shift(1) > dataframe['slowd'].shift(1))
             ),
-            'enter_long'] = 1
+            'exit_long'] = 1
+
         return dataframe
